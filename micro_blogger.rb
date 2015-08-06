7232d8f0b491701d35c9ca5ef1e37056a4cff200
ftmatsumoto <felipetmatsumoto@yahoo.com.br>
2015-08-06 15:06:45 -0300

    iteration 5

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,4 +1,5 @@
 require 'jumpstart_auth'
+require 'bitly'
 
 class MicroBlogger
 	attr_reader :client
@@ -22,6 +23,8 @@ class MicroBlogger
 				when 'dm' then dm(parts[1], parts[2..-1].join(" "))
 				when 'spam' then spam_my_followers(parts[1..-1].join(" "))
 				when 'elt' then everyones_last_tweet
+				when 's' then shorten(parts[-1])
+				when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
 			else
 				puts "Sorry, I don't know how to #{command}"
 			end
@@ -75,6 +78,14 @@ class MicroBlogger
       printf "#{tweet} at #{timestamp}"
       puts ''
     end
+	end
+
+	def shorten(original_url)
+		Bitly.use_api_version_3
+		# Shortening Code
+		bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+		puts "Shortening this URL: #{original_url}"
+		bitly.shorten(original_url).short_url
 	end
 
 end
